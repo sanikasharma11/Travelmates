0588ad1b6587602e70f92fe3c3e15ddcd7801607
Govind <govind@Govinds-MacBook-Pro.local>
2024-08-21 15:08:02 +0530

    [Added] Structure

---
+++ travel/screen/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Detail')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
